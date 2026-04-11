ed8b6f0c7aab91e4c67c0519adf41c714edd543d
JaniorB <bjaniorandres@gmail.com>
2026-04-10 20:47:41 -0600

    Añadido Struct de jugar y de cartas criatura

---
+++ main.c
@@ -0,0 +1,43 @@
+// Created by JaniorB on 4/8/2026.
+#include <stdio.h>
+#include <stdlib.h>
+#define nameMax 20
+
+struct jugador {
+    char nombre[nameMax];
+    int nivel = 0;
+    bool orden;
+};
+
+typedef struct criatura {
+    char nombre[nameMax];
+    char tipo[nameMax];
+    int nivel;
+    int hp;
+    int ataque;
+};
+
+criatura lobo{
+    char nombre[nameMax] "Lobo Plateado";
+    char[ tipo[nameMax] = "atacante";
+    int nivel = 1
+    int hp = 3;
+    int ataque = 2;
+};
+
+criatura tortuga{
+    char nombre[nameMax] = "Tortuga Terrestre";
+    char tipo[nameMax] = "tanque";
+    int nivel = 1;
+    int hp = 5;
+    int ataque = 0;
+}
+
+void fusionar();
+
+int main() {
+
+}
+
+
+
