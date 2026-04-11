b02f5f350b65113552f538258b69fdcc9dbf4a85
Sbrentor <githubpersonal914@gmail.com>
2026-04-10 22:34:52 -0600

    FIX - arreglo de estructuras y llamado de estas

--- main.c
+++ main.c
@@ -1,40 +1,46 @@
 // Created by JaniorB on 4/8/2026.
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #define nameMax 20
 
-struct jugador {
+//S43G 4/10/2026
+typedef struct{
     char nombre[nameMax];
-    int nivel = 0;
+    int nivel;
     bool orden;
-};
+} jugador;
 
-typedef struct criatura {
+typedef struct  {
     char nombre[nameMax];
     char tipo[nameMax];
     int nivel;
     int hp;
     int ataque;
-};
+}criatura;
 
-criatura lobo{
-    char nombre[nameMax] "Lobo Plateado";
-    char[ tipo[nameMax] = "atacante";
-    int nivel = 1
-    int hp = 3;
-    int ataque = 2;
+void StatsCriaturasIniciales() {
+    criatura lobo = {
+        "Lobo Plateado",
+        "atacante",
+        1,
+        3,
+        2
+    };
 
-};
-
-criatura tortuga{
-    char nombre[nameMax] = "Tortuga Terrestre";
-    char tipo[nameMax] = "tanque";
-    int nivel = 1;
-    int hp = 5;
-    int ataque = 0;
+    criatura tortuga = {
+        "Tortuga Terrestre",
+        "tanque",
+        1,
+        5,
+        0
+    };
 }
 
 void fusionar();
+
+//funcion donde inciara el juego
+void Boot();
 
 int main() {
 
